3ee339269a87a183655a0e008447ebe3e08e8ce2
rusakov.vv <rusakovslava@gmail.com>
2019-10-12 23:36:30 +0300

    Check

--- app/Telegram/WeatherCommand.php
+++ app/Telegram/WeatherCommand.php
@@ -2,6 +2,11 @@
 
 namespace App\Telegram;
 
+use DateTime;
+use DateTimeZone;
+use Exception;
+use GuzzleHttp\Client;
+use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
 
 class WeatherCommand extends Command
@@ -14,19 +19,28 @@ class WeatherCommand extends Command
     /**
      * @var string Command Description
      */
-    protected $description = 'Weather command, Get a list of commands';
+    protected $description = 'Weather command, Get weather in Moscow';
 
     /**
      * {@inheritdoc}
+     * @throws Exception
      */
     public function handle($arguments)
     {
-        $commands = $this->telegram->getCommands();
+        $this->replyWithChatAction(['action' => Actions::TYPING]);
 
+        $client = new Client();
+        $uri = sprintf('%s524901&units=metric&APPID=%s', env('OPENWEATHERMAP_URI'), env('OPENWEATHERMAP_KEY'));
+        $data = $client->get($uri);
+        $weather = json_decode($data->getBody());
         $text = '';
-        foreach ($commands as $name => $handler) {
-            $text .= sprintf('/%s - %s' . PHP_EOL, $name, $handler->getDescription());
-        }
+        $text .= sprintf('Погода в Москве: %s C %s' . PHP_EOL, $weather->main->temp, $weather->weather[0]->main);
+        $sunrise = new DateTime(date("Y-m-d H:i:s", $weather->sys->sunrise));
+        $sunrise->setTimezone(new DateTimeZone("+3"));
+        $text .= sprintf('Рассвет: %s' . PHP_EOL, $sunrise->format("Y-m-d H:i:s"));
+        $sunset = new DateTime(date("Y-m-d H:i:s", $weather->sys->sunset));
+        $sunset->setTimezone(new DateTimeZone("+3"));
+        $text .= sprintf('Закат: %s' . PHP_EOL,   $sunset->format("Y-m-d H:i:s"));
 
         $this->replyWithMessage(compact('text'));
     }
