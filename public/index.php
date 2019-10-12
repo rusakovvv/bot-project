47cb8ac609bf78df4ad6342351f868dd7cce7fbd
rusakov.vv <rusakovslava@gmail.com>
2019-10-12 22:36:07 +0300

    Check

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, OPTIONS");
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
